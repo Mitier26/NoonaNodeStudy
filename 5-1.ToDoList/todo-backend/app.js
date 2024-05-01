5e6e7ce04465e7b2315f966c2f5a4a69391c4812
Molkang <mitier26@gmail.com>
2024-05-01 23:46:00 +0900

    ToDo List

--- 5-1.ToDoList/todo-backend/app.js
+++ 5-1.ToDoList/todo-backend/app.js
@@ -2,14 +2,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
+require('dotenv').config();
 const cors = require('cors');
 
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/api", indexRouter);
 
-const mongoURI = 'mongodb://localhost:27017/todo-demo2';
+// const mongoURI = 'mongodb://localhost:27017/todo-demo2';
+const mongoURI = MONGODB_URI_PROD;
 
 mongoose.connect(mongoURI, { useNewUrlParser: true }).then(() => {
     console.log("mongoose connected");
@@ -17,6 +20,6 @@ mongoose.connect(mongoURI, { useNewUrlParser: true }).then(() => {
     console.log("DB connection fail", error);
 });
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("server on 5000");
 })
