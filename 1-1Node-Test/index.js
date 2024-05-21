b0eb54c6998ac44a300eb791aa5d69f416c0b8a6
Molkang <mitier26@gmail.com>
2024-05-21 13:44:10 +0900

    NodeJs란

---
+++ 1-1Node-Test/index.js
@@ -0,0 +1,13 @@
+console.log("1. 물주기");
+
+setTimeout(() => {
+    console.log("2. 밥주기");
+}, 3000)
+
+setTimeout(() => {
+    console.log("3. 햄버거주기");
+}, 2000)
+
+console.log("계산 하기");
+
+// 빠르게 출력되는 것이 먼저 나온다.
