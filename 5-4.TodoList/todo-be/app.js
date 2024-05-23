b9537b038bf5348b710ce4aa8a39186640d2e3c3
Molkang <mitier26@gmail.com>
2024-05-23 21:32:53 +0900

    수정

--- 5-4.TodoList/todo-be/app.js
+++ 5-4.TodoList/todo-be/app.js
@@ -8,6 +8,10 @@ const indexRouter = require("./routes/index");
 const cors = require("cors");
 
 const app = express();
+
+require('dotenv').config();
+const MONGODB_URI_PROD = process.env.MONGODB_URI_PROD;
+
 app.use(bodyParser.json());
 
 app.use(cors());
@@ -16,7 +20,10 @@ app.use(cors());
 app.use("/api", indexRouter);
 // /api -> index -> tasks -> router
 
-const mongoURI = "mongodb://localhost:27017/todo-demo";
+// 이것은 로컬 주소, Altass 를 이용하면서 필요없어졌다.
+// const mongoURI = "mongodb://localhost:27017/todo-demo";
+// env를 사용해야하는데 이것은 설치해 주어야한다 npm i dotenv
+const mongoURI = MONGODB_URI_PROD;
 
 // 연결
 mongoose.connect(mongoURI, { useNewUrlParser: true }).then(() => {
@@ -26,6 +33,6 @@ mongoose.connect(mongoURI, { useNewUrlParser: true }).then(() => {
 });
 
 // 5000 번으로 들어오면 작동한다.
-app.listen(5000, () => {
-    console.log("서버 5000번");
-});
+app.listen(process.env.PORT || 5000, () => {
+    console.log("server on 5000");
+})
