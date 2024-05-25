df3e92f4186284aa7038a40b8c25949618a8e029
Molkang <mitier26@gmail.com>
2024-05-25 10:03:55 +0900

    todoboard 변경

--- 5-4.TodoList/todo-fe/src/components/TodoBoard.js
+++ 5-4.TodoList/todo-fe/src/components/TodoBoard.js
@@ -5,13 +5,11 @@ const TodoBoard = ({ todoList, deleteItem, toggleComplete }) => {
   return (
     <div>
       <h2>Todo List</h2>
-      {todoList.length > 0 ? todoList.map((item, index) => (
+      {todoList.length > 0 && todoList.map((item, index) => (
         <TodoItem item={item}
           key={index}
           deleteItem={deleteItem}
-          toggleComplete={toggleComplete} />)) : <h2>There is no Item to show</h2>}
-      {/* <TodoItem/> will be here once we get the todoList */}
-      {/* <h2>There is no Item to show</h2> */}
+          toggleComplete={toggleComplete} />))}
     </div>
   );
 };
